9ecefa94ca654c8b0377cd1c50df795fd08353ab
John Lins <johnpaullins@gmail.com>
2021-10-15 19:43:12 -0700

    pure

--- nn.c
+++ nn.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+
 
 #define e 2.71
 #define n 3
+#define l 3
 
 
 float sigmoid(float v){
@@ -13,7 +18,7 @@ float sigmoid_d(float v){
     return sigmoid(v) * (1-sigmoid(v));
 }
 
-float dot(float w[3], float x[3]){
+float dot(float w[l], float x[l]){
     float out = 0;
     for(int i = 0; i<n; i++){
         out += w[i] * x[i];
@@ -21,11 +26,11 @@ float dot(float w[3], float x[3]){
    return out;
 }
 
-float forward(float w[3], float x[3]){
+float forward(float w[l], float x[l]){
     return sigmoid(dot(w, x));
 }
 
-float loss_partial(int r, float w[3], float t_x[n][3], float t_y[n]){
+float loss_partial(int r, float w[l], float t_x[n][l], float t_y[n], float (*s)(), float (*f)()){
     float p = 0;
     for(int i = 0; i < n; i++){
         float t = forward(w, t_x[i]);
@@ -36,41 +41,50 @@ float loss_partial(int r, float w[3], float t_x[n][3], float t_y[n]){
     return p / n;
 }
 
-float MSE(float w[3], float t_x[n][3], float t_y[n]){
+float MSE(float w[l], float t_x[n][l], float t_y[n], float (*f)()){
 
     float out = 0;
     for(int i = 0; i < n; i++){
-        out += powf((forward(w, t_x[i]) - t_y[i]), 2);
+        out += powf(((*f)(w, t_x[i]) - t_y[i]), 2);
     }
 
     return out / n;
 
 }
 
+void train(int *iterations, float *lr, float (*lp)(), float (*f)(), float w[l], float t_x[n][l], float t_y[n]){
+
+    for(int i = 0; i < *iterations; i++){
+
+        for(int j = 0; j < 3; j++){
+            w[j] = w[j] - (*lr) * (*lp)(j, w, t_x, t_y, f);
+        }
+
+        printf("loss: %f \n", MSE(w, t_x, t_y, f));
+
+    }
+
+}
+
 
 int main()
 {
-    float w[3] = {.3, -.9, .1};
+
+    srand(time(0));
+
+    float w[l] = {.3, -.9, .1};
+
+    for(int i = 0; i < l; i++){
+        w[i] = 1.0 * rand() / (RAND_MAX / 2) - 1;
+    }
    
-    float t_x[3][n] = {{1, 0, 0},{1, 1, 0},{0, 1, 0}};
+    float t_x[n][l] = {{1, 0, 0},{1, 1, 0},{0, 1, 0}};
     float t_y[n] = {1, 1, 0};
    
-   
-    //forward
-   printf("%f", forward(w, t_x[0]));
-
-   //train
     int iterations = 10000;
     float learning_rate = .01;
-    for(int i = 0; i < iterations; i++){
-
-        for(int j = 0; j < 3; j++){
-            w[j] = w[j] - learning_rate * loss_partial(j, w, t_x, t_y);
-        }
-
-        printf("loss: %f \n", MSE(w, t_x, t_y));
-
-    }
+    
+    train(&iterations, &learning_rate, &loss_partial, &forward, w, t_x, t_y);
 
 
     float test1[3] = {1, 0, 0};
